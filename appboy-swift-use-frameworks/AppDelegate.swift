c82a9c0310a293902b581b3fa7359a20726b7a94
Kyle Fritz <kyle.p.fritz@gmail.com>
2015-10-14 15:09:00 -0400

    not sure what's up

---
+++ appboy-swift-use-frameworks/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  appboy-swift-use-frameworks
+//
+//  Created by Kyle Fritz on 10/14/15.
+//
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        Appboy.startWithApiKey("YOUR-API-KEY-HERE", inApplication: application, withLaunchOptions: launchOptions)
+
+        return true
+    }
+}
---
+++ appboy-swift-use-frameworks/bridgingheader.h
@@ -0,0 +1,9 @@
+//
+//  bridgingheader.h
+//  appboy-swift-use-frameworks
+//
+//  Created by Kyle Fritz on 10/14/15.
+//
+//
+
+#import "Appboy_iOS_SDK/AppboyKit.h"
